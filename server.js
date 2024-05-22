9db731777f79deca11d6c94f842108ef6d0f7d2c
inwapi <qinwcom.com@gmail.com>
2024-05-22 11:27:05 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
